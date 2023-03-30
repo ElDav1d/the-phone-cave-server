2a49d0aabdbff8e5accfd359b85950014ac0fdb7
ElDav1d <dvivoes@yahoo.es>
2023-03-30 17:02:51 +0200

    create get routes

---
+++ routes/phones.routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Phone = require("../models/Phone.model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await Phone.find();
+
+    res.status(200).json(response);
+  } catch (error) {
+    next.error;
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const response = await Phone.findById(req.params.id);
+    console.log;
+    res.status(200).json(response);
+  } catch (error) {
+    next.error;
+  }
+});
+
+module.exports = router;
